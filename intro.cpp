3d309d158b6841e7ea8994420df75df578af6ee0
sharkie <sharkie@arch.com>
2023-01-04 13:54:36 +0100

    initial commit

---
+++ intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int x, y;
+    int sum;
+    std::cout << "type a number: ";
+    std::cin >> x;
+    std::cout << "type another number: ";
+    std::cin >> y;
+    sum = x + y;
+    std::cout << "Sum is: " << sum;
+    }
